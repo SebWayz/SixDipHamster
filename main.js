5834141247c1dc6ddbbd8f4b786edc99d7c0edbf
Sébastien Bougouin <sebway377@gmail.com>
2021-08-29 00:04:41 +0200

    First version achievement loading system

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 // Loads side navbar & footer on the page to create a template
 $(function(){
     $("#navbar").load("navbar.html");
-    $("#footer").load("footer.html"); 
+    $("#footer").load("footer.html");
 });
 
 var achievementBtn = document.getElementById('invitational');
+var brutalTourneyBtn = document.getElementById('brutal');
+var eventSection = document.getElementById('event');
 
-achievementBtn.addEventListener('click', () => {
-    achievementBtn.innerHTML = '<h1> POGGERS </h1>';
+$('#invitational').click(function () {
+    $('#event').load("ChuTv_invitational_desc.html").fadeIn('slow');
+});
+
+$('#brutal').click(function () {
+    $('#event').load("BG_2v2_desc.html").fadeIn('slow');
 });
